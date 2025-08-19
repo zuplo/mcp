ad8b687b1158504d09bf2e625081e1fee25d3b19
John McBride <john@zuplo.com>
2025-08-19 11:07:12 -0400

    feat: OpenAPI extension support (#38)
    Signed-off-by: John McBride <john@zuplo.com>

---
+++ src/openapi/types.ts
@@ -0,0 +1,64 @@
+/**
+ * OpenAPI MCP server metadata support via "x-mcp-server" extension.
+ *
+ * @example
+ * ```json
+ * {
+ *   "paths": {
+ *     "mcp": {
+ *       "post: {
+ *         "name": "my MCP server name",
+ *         "version": "1.2.3",
+ *         "tools": [
+ *           {
+ *             "name": "get_all_users",
+ *             "description": "Tool for getting all users in the system"
+ *           },
+ *           {
+ *             "name": "create_new_user",
+ *             "description": "Tool for creating a new user",
+ *             "inputSchema": { ... }
+ *           }
+ *         ]
+ *       }
+ *     }
+ *   }
+ * }
+ * ```
+ */
+export interface ExtensionMcpServer {
+  /**
+   * The name of the MCP server itself: this is advertised during MCP initialization
+   */
+  name: string;
+
+  /**
+   * The version of the MCP server itself: this is advertised during MCP initialization
+   */
+  version: string;
+
+  /**
+   * Array of tool metadata in the server
+   */
+  tools: ExtensionMcpServerTool[];
+}
+
+/**
+ * Individual tool metadata for a "x-mcp-server" tool list
+ */
+export interface ExtensionMcpServerTool {
+  /**
+   * The name of the MCP tool
+   */
+  name: string;
+
+  /**
+   * The description of the MCP tool
+   */
+  description: string;
+
+  /**
+   * JSON Schema for the tool's input parameters
+   */
+  inputSchema?: object;
+}
